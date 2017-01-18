1d3371d53daea94fb8fe90398d7552263ce2081f
lkq <626883409@qq.com>
2017-01-18 13:50:42 +0800

    LeetCode 练习

--- src/AddTwoNumbers/AddTwoNumbers.java
+++ src/AddTwoNumbers/AddTwoNumbers.java
@@ -46,4 +46,28 @@ public class AddTwoNumbers {
         public void getNext(ListNode l){
             System.out.println(l.next.val);
         }
+
+
+        /*
+        * 网站上给出的做法
+        */
+       /* public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode dummyHead = new ListNode(0);
+            ListNode p = l1, q = l2, curr = dummyHead;
+            int carry = 0;
+            while (p != null || q != null) {
+                int x = (p != null) ? p.val : 0;
+                int y = (q != null) ? q.val : 0;
+                int sum = carry + x + y;
+                carry = sum / 10;
+                curr.next = new ListNode(sum % 10);
+                curr = curr.next;
+                if (p != null) p = p.next;
+                if (q != null) q = q.next;
+            }
+            if (carry > 0) {
+                curr.next = new ListNode(carry);
+            }
+            return dummyHead.next;
+        }*/
 }
