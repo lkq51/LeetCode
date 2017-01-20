81c04a8d0ec10eaf1bd1e1979329b9d38da6e583
lkq <626883409@qq.com>
2017-01-20 17:00:11 +0800

    LeetCode 练习

---
+++ src/LongestSubstringWithoutRepeatingCharacters/Solution.java
@@ -0,0 +1,17 @@
+package LongestSubstringWithoutRepeatingCharacters;
+
+/**
+ * Created by lkq on 2017/1/20.
+ */
+public class Solution {
+    public int lengthOfLongestSubstring(String s){
+        int i = 0;
+        char[] arr = s.toCharArray();
+        System.out.println(arr);
+        return i;
+    }
+
+    public static void main(String[] args) {
+        new Solution().lengthOfLongestSubstring("asdf");
+    }
+}
